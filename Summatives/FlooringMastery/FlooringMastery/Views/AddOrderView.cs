f3270359ff4bd0406d0143ef2d44dedea1eda4b2
willis81808 <willis81808@gmail.com>
2020-02-13 15:56:58 -0700

    Formatting improvements for the "Add" and "Edit" view classes

--- Summatives/FlooringMastery/FlooringMastery/Views/AddOrderView.cs
+++ Summatives/FlooringMastery/FlooringMastery/Views/AddOrderView.cs
@@ -15,57 +15,24 @@ namespace FlooringMastery.Views
             DataManager repository = DataManagerFactory.Create();
 
             Console.Clear();
-
+            
             // date
             DateTime date = ConsoleIO.GetDateTime("Order date (must be a future date):", false);
             Console.WriteLine();
 
-            // customer name
+            // name
             Console.WriteLine("Customer name:");
             Console.Write("> ");
             string name = Console.ReadLine();
             Console.WriteLine();
-
+            
             // state
-            Console.WriteLine("State:");
-            Console.Write("> ");
-            string stateText = Console.ReadLine();
-            State state = (from s in repository.States
-                           where s.StateAbbreviation == stateText || s.StateName == stateText
-                           select s).FirstOrDefault();
-            if (state == null)
-            {
-                Console.WriteLine($"We do not do business in {stateText}, unfortunately we cannot accept this order.");
-                Console.Write("Press any key to return to the main menu...");
-                Console.ReadKey();
-                return;
-            }
+            State state = GetStateFrom(repository.States);
+            if (state == null) return;
             Console.WriteLine();
-
+            
             // product
-            Product product = null;
-            do
-            {
-                for (int i = 0; i < repository.Products.Length; i++)
-                {
-                    Console.WriteLine($"{i+1})");
-                    ConsoleIO.DisplayProductDetails(repository.Products[i]);
-                    Console.WriteLine();
-                }
-                int selection = ConsoleIO.GetInteger("Enter a product by number:") - 1;
-                if (selection < repository.Products.Length && selection >= 0)
-                {
-                    product = repository.Products[selection];
-                }
-                else
-                {
-                    Console.WriteLine($"Invalid selection: {selection + 1}");
-                    Console.WriteLine($"Expected a number in the range 1-{repository.Products.Length}");
-                    Console.Write("Press any key to try again...");
-                    Console.ReadKey();
-                }
-            }
-            while (product == null);
+            Product product = GetProductsFrom(repository.Products);
             Console.WriteLine();
 
             // area
@@ -80,6 +47,52 @@ namespace FlooringMastery.Views
             {
                 repository.AddOrder(date, order);
             }
+        }
+        
+        private State GetStateFrom(State[] states)
+        {
+            Console.WriteLine("State:");
+            Console.Write("> ");
+            string stateText = Console.ReadLine();
+            State state = (from s in states
+                           where s.StateAbbreviation == stateText || s.StateName == stateText
+                           select s).FirstOrDefault();
+            if (state == null)
+            {
+                Console.WriteLine($"We do not do business in {stateText}, unfortunately we cannot accept this order.");
+                Console.Write("Press any key to return to the main menu...");
+                Console.ReadKey();
+                return null;
+            }
+            return state;
+        }
+
+        private Product GetProductsFrom(Product[] products)
+        {
+            Product product = null;
+            do
+            {
+                for (int i = 0; i < products.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1})");
+                    ConsoleIO.DisplayProductDetails(products[i]);
+                    Console.WriteLine();
+                }
+                int selection = ConsoleIO.GetInteger("Enter a product by number:") - 1;
+                if (selection < products.Length && selection >= 0)
+                {
+                    product = products[selection];
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid selection: {selection + 1}");
+                    Console.WriteLine($"Expected a number in the range 1-{products.Length}");
+                    Console.Write("Press any key to try again...");
+                    Console.ReadKey();
+                }
+            }
+            while (product == null);
+            return product;
         }
     }
 }
--- Summatives/FlooringMastery/FlooringMastery/Views/EditOrderView.cs
+++ Summatives/FlooringMastery/FlooringMastery/Views/EditOrderView.cs
@@ -18,122 +18,120 @@ namespace FlooringMastery.Views
             Console.Clear();
             DateTime targetDate = ConsoleIO.GetDateTime("Enter the date of the order you'd like to edit:");
 
-            if (repository.Orders.ContainsKey(targetDate))
-            {
-                int orderNumber = ConsoleIO.GetInteger("Enter the order number of the order to edit:");
-                OrderLookupResponse result = repository.LookupOrder(targetDate, orderNumber);
-                if (result.Success)
-                {
-                    Console.WriteLine();
-
-                    // customer name
-                    Console.WriteLine($"Customer name ({result.Order.CustomerName}):");
-                    Console.Write("> ");
-                    string name = Console.ReadLine();
-                    Console.WriteLine();
-
-                    // state
-                    Console.WriteLine($"State ({result.Order.State}):");
-                    Console.Write("> ");
-                    string stateText;
-                    State state = null;
-                    do
-                    {
-                        stateText = Console.ReadLine();
-                        if (stateText != "")
-                        {
-                            state = (from s in repository.States
-                                     where s.StateAbbreviation == stateText || s.StateName == stateText
-                                     select s).FirstOrDefault();
-                            if (state == null)
-                            {
-                                Console.WriteLine($"We do not do business in {stateText}.");
-                            }
-                        }
-                        else
-                        {
-                            state = (from s in repository.States
-                                     where s.StateAbbreviation == result.Order.State
-                                     select s).FirstOrDefault();
-                        }
-                    }
-                    while (state == null);
-                    Console.WriteLine();
-
-                    // product
-                    Product product = null;
-                    do
-                    {
-                        for (int i = 0; i < repository.Products.Length; i++)
-                        {
-                            Console.WriteLine($"{i + 1})");
-                            ConsoleIO.DisplayProductDetails(repository.Products[i]);
-                            Console.WriteLine();
-                        }
-                        Console.WriteLine($"Enter a product by number ({result.Order.ProductType}):");
-                        Console.Write("> ");
-                        if (int.TryParse(Console.ReadLine(), out int selection))
-                        {
-                            if (selection <= repository.Products.Length && selection >= 0)
-                            {
-                                product = repository.Products[selection-1];
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Invalid selection: {selection}");
-                                Console.WriteLine($"Expected a number in the range 1-{repository.Products.Length}");
-                            }
-                        }
-                        else
-                        {
-                            product = (from p in repository.Products
-                                       where p.ProductType == result.Order.ProductType
-                                       select p).FirstOrDefault();
-                        }
-                    }
-                    while (product == null);
-                    Console.WriteLine();
-
-                    // area
-                    decimal? area = null;
-                    do
-                    {
-                        Console.WriteLine($"Floor area ({result.Order.Area}):");
-                        Console.Write("> ");
-                        string areaText = Console.ReadLine();
-                        if (areaText == "")
-                        {
-                            area = result.Order.Area;
-                        }
-                        else if (decimal.TryParse(areaText, out decimal value))
-                        {
-                            area = value;
-                        }
-                    }
-                    while (!area.HasValue);
-                    Console.WriteLine();
-
-                    // summary
-                    Console.Clear();
-                    Order order = new Order(name == "" ? result.Order.CustomerName : name, state, product, area.Value);
-                    ConsoleIO.DisplayOrderDetails(order);
-                    if (ConsoleIO.GetBool("Would you like to save this order?", "Y", "N", false))
-                    {
-                        repository.UpdateOrder(order, targetDate, orderNumber);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(result.Message);
-                    Console.Write("Press any key to return to main menu...");
-                    Console.ReadKey();
-                }
-            }
-            else
+            if (!repository.Orders.ContainsKey(targetDate))
             {
                 Console.WriteLine($"No orders were found on {targetDate.ToShortDateString()}");
                 Console.Write("Press any key to return to main menu...");
                 Console.ReadKey();
+                return;
+            }
+
+            int orderNumber = ConsoleIO.GetInteger("Enter the order number of the order to edit:");
+            OrderLookupResponse result = repository.LookupOrder(targetDate, orderNumber);
+            if (!result.Success)
+            {
+
+                Console.WriteLine(result.Message);
+                Console.Write("Press any key to return to main menu...");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine();
+
+            // customer name
+            Console.WriteLine($"Customer name ({result.Order.CustomerName}):");
+            Console.Write("> ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            // state
+            Console.WriteLine($"State ({result.Order.State}):");
+            Console.Write("> ");
+            string stateText;
+            State state = null;
+            do
+            {
+                stateText = Console.ReadLine();
+                if (stateText != "")
+                {
+                    state = (from s in repository.States
+                             where s.StateAbbreviation == stateText || s.StateName == stateText
+                             select s).FirstOrDefault();
+                    if (state == null)
+                    {
+                        Console.WriteLine($"We do not do business in {stateText}.");
+                    }
+                }
+                else
+                {
+                    state = (from s in repository.States
+                             where s.StateAbbreviation == result.Order.State
+                             select s).FirstOrDefault();
+                }
+            }
+            while (state == null);
+            Console.WriteLine();
+
+            // product
+            Product product = null;
+            do
+            {
+                for (int i = 0; i < repository.Products.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1})");
+                    ConsoleIO.DisplayProductDetails(repository.Products[i]);
+                    Console.WriteLine();
+                }
+                Console.WriteLine($"Enter a product by number ({result.Order.ProductType}):");
+                Console.Write("> ");
+                if (int.TryParse(Console.ReadLine(), out int selection))
+                {
+                    if (selection <= repository.Products.Length && selection >= 0)
+                    {
+                        product = repository.Products[selection - 1];
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid selection: {selection}");
+                        Console.WriteLine($"Expected a number in the range 1-{repository.Products.Length}");
+                    }
+                }
+                else
+                {
+                    product = (from p in repository.Products
+                               where p.ProductType == result.Order.ProductType
+                               select p).FirstOrDefault();
+                }
+            }
+            while (product == null);
+            Console.WriteLine();
+
+            // area
+            decimal? area = null;
+            do
+            {
+                Console.WriteLine($"Floor area ({result.Order.Area}):");
+                Console.Write("> ");
+                string areaText = Console.ReadLine();
+                if (areaText == "")
+                {
+                    area = result.Order.Area;
+                }
+                else if (decimal.TryParse(areaText, out decimal value))
+                {
+                    area = value;
+                }
+            }
+            while (!area.HasValue);
+            Console.WriteLine();
+
+            // summary
+            Console.Clear();
+            Order order = new Order(name == "" ? result.Order.CustomerName : name, state, product, area.Value);
+            ConsoleIO.DisplayOrderDetails(order);
+            if (ConsoleIO.GetBool("Would you like to save this order?", "Y", "N", false))
+            {
+                repository.UpdateOrder(order, targetDate, orderNumber);
             }
         }
     }
